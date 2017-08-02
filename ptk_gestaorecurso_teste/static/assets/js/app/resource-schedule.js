19f46fe622c148fb212bbbf5a5881583a6c0b7f0
Danilo Ambrosio <danilo.ambrosio@focusnetworks.com.br>
2017-08-01 22:18:49 -0300

    Resource Schedule Landing page

--- ptk_gestaorecurso_teste/static/assets/js/app/resource-schedule.js
+++ ptk_gestaorecurso_teste/static/assets/js/app/resource-schedule.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+  var resourceScheduleUrlTemplate = "https://calendar.google.com/calendar/embed?src=:schedule-source:&ctz=America/Sao_Paulo";
+
   $('.datepicker').datepicker({
     autoclose: true
   });
@@ -7,6 +9,16 @@ $(document).ready(function(){
   $('#schedule-btn').click(function(){
     $('#resource-schedule-modal').modal('hide');
     toastr.info('Recurso agendado com sucesso!','');
+  });
+
+  $('#resource-calendar').change(function(){
+    if(!$(this).val()) {
+      $('.calendar-box').hide();
+    }
+    var selectedResourceCalendar = $(this).val();
+    var calendarUrl = resourceScheduleUrlTemplate.replace(':schedule-source:', selectedResourceCalendar);
+    $('#calendar-frame').attr('src', calendarUrl);
+    $('.calendar-box').show();
   });
 
 });
